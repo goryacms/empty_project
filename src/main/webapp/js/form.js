572455f24b8bb78dfd7d85e27d4afa269935996e
goryacms <gorjachev_maksim@mail.ru>
2018-03-18 21:13:44 +0300

    Создал форму для запросов

--- src/main/webapp/js/form.js
+++ src/main/webapp/js/form.js
@@ -1,53 +1,110 @@
 $(document).ready(function () {
-    $("#ping").click(function () {
-        $.ajax({
-            url:"/ping",
-            type:"GET",
-            contentType:"application/json; charset=utf-8",
-            dataType:"text",
-            success: function(result){
-                console.log(result);
-                alert(result);
-            }
-        });
-    });
+        $("#addOrg").click(function () {
+            console.log('ORGANIZATION');
+            var org = {
+                name: $("#name").val(),
+                fullName: $("#fullName").val(),
+                inn: $("#inn").val(),
+                kpp: $("#kpp").val(),
+                address: $("#address").val(),
+                phone: $("#phone").val(),
+                isActive: $("#isActive").val() == 1 ? true : false
+            };
 
-    $("#addPerson").click(function () {
-        console.log('PERSON');
-        var person = {
-            name: $("#name").val(),
-            age: $("#age").val()
-        };
-        console.log('PER', person);
+            console.log('ORG', org);
 
-        $.ajax({
-            url:"/person",
-            type:"POST",
-            data: JSON.stringify(person),
-            contentType:"application/json; charset=utf-8",
-            success: function(){
-                alert('Success');
-            }
+            $.ajax({
+                url:"/api/organization/save",
+                type:"POST",
+                data: JSON.stringify(org),
+                contentType:"application/json; charset=utf-8",
+                success: function(result){
+                    console.log(result);
+                    alert(result);
+                }
+            });
+
         });
 
-        clearFields();
-    });
 
-    $("#personList").click(function () {
-        $.ajax({
-            url:"/person",
-            type:"GET",
-            contentType:"application/json; charset=utf-8",
-            dataType:"json",
-            success: function(result){
-                console.log(result);
-                alert(JSON.stringify(result));
-            }
+
+        $("#updOrg").click(function () {
+            console.log('ORGANIZATION UPDATE');
+            var org = {
+                id: $("#id").val(),
+                name: $("#name").val(),
+                fullName: $("#fullName").val(),
+                inn: $("#inn").val(),
+                kpp: $("#kpp").val(),
+                address: $("#address").val(),
+                phone: $("#phone").val(),
+                isActive: $("#isActive").prop( "checked" )
+            };
+
+            console.log('ORG', org);
+
+            $.ajax({
+                url:"/api/organization/update",
+                type:"POST",
+                data: JSON.stringify(org),
+                contentType:"application/json; charset=utf-8",
+                success: function(result){
+                    console.log(result);
+                    alert(result);
+                }
+            });
+
         });
-    });
+
+
+
+        $("#listOrg").click(function () {
+            console.log('ORGANIZATION LIST');
+            var org = {
+                name: $("#name").val(),
+                inn: $("#inn").val(),
+                isActive: $("#isActive").val() == 1 ? true : false
+            };
+
+            console.log('ORG', org);
+
+            $.ajax({
+                url:"/api/organization/list",
+                type:"POST",
+                data: JSON.stringify(org),
+                contentType:"application/json; charset=utf-8",
+                success: function(result){
+                    console.log(result);
+                    alert(result);
+                }
+            });
+
+        });
+
+
+        $("#delOrg").click(function () {
+            console.log('ORGANIZATION DELETE');
+            var org = {
+                id: $("#id").val()
+            };
+
+            console.log('ORG', org);
+
+            $.ajax({
+                url:"/api/organization/list",
+                url:"/api/organization/delete",
+                type:"POST",
+                data: JSON.stringify(org),
+                contentType:"application/json; charset=utf-8",
+                success: function(result){
+                    console.log(result);
+                    alert(result);
+                }
+            });
+
+        });
+
+
+
+
 });
-
-var clearFields = function () {
-    $("#name").val('');
-    $("#age").val('');
-};
