6412a7cbdb8b5793afa53a70a4375dfc981e1656
goryacms <gorjachev_maksim@mail.ru>
2018-03-24 10:45:13 +0300

    добавил save

--- src/test/java/ru/bellintegrator/practice/users/dao/UserDAOTest.java
+++ src/test/java/ru/bellintegrator/practice/users/dao/UserDAOTest.java
@@ -17,6 +17,8 @@ import ru.bellintegrator.practice.guides.dao.DocDAO;
 import ru.bellintegrator.practice.guides.model.Citizenship;
 import ru.bellintegrator.practice.guides.model.Doc;
 import ru.bellintegrator.practice.guides.model.DocUser;
+import ru.bellintegrator.practice.office.dao.OfficeDAO;
+import ru.bellintegrator.practice.office.model.Office;
 import ru.bellintegrator.practice.users.model.User;
 
 //import java.util.HashSet;
@@ -24,6 +26,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 //import java.util.Set;
 
 @RunWith(SpringRunner.class)
@@ -37,6 +40,9 @@ public class UserDAOTest {
     private UserDAO userDAO;
 
     @Autowired
+    private OfficeDAO officeDAO;
+
+    @Autowired
     private DocDAO docDAO;
 
     @Autowired
@@ -46,6 +52,8 @@ public class UserDAOTest {
     private static final int INSERT_COUNT_FROM_DB  = 21;
     private static final int DELETE_COUNT_FROM_DB  = 19;
 
+
+    private static final SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
     /**
      * Проверка delete
      */
@@ -91,7 +99,6 @@ public class UserDAOTest {
 
         String d1 = "21.10.2012";
         String d2 = "24.12.2011";
-        SimpleDateFormat format = new SimpleDateFormat("dd.mm.yyyy");
 
         Date regDt = null; Date docDt = null;
         try {
@@ -101,9 +108,6 @@ public class UserDAOTest {
             e.printStackTrace();
         }
 
-
-
-
         us.setRegistrationDate(regDt);
         us.setPhone("35-15-90");
 
@@ -112,7 +116,7 @@ public class UserDAOTest {
         docUser.setDocDate(docDt);
         docUser.setDocNumber(12123165115165165L);
 
-        Doc doc = docDAO.loadById(1); // Это же справочник документов, значит берём из базы
+        Doc doc = docDAO.loadById(21); // Это же справочник документов, значит берём из базы
 
         docUser.setDocs(doc);
 
@@ -129,8 +133,89 @@ public class UserDAOTest {
 
         Assert.assertEquals("Иван", us1.getFirstName());
         Assert.assertEquals("Драматург", us1.getPosition());
+    }
 
+    /**
+     * loadByParams
+     */
+    @Test
+    public void loadByParams(){
+        User us = new User();
 
+        Office office = new Office();
+        office.setId(8L);
+
+        us.setOffice(office);
+
+        us.setFirstName("Никита");
+        us.setLastName("Селезнёв");
+        us.setMiddleName("Алек");
+
+        List<User> us1 = userDAO.loadByParams(us);
+        Assert.assertEquals("Программист", us1.get(0).getPosition());
+    }
+
+    /**
+     * save
+     */
+    @Test
+    public void save(){
+        User us = new User();
+
+        Office office = officeDAO.loadById(4L);
+        us.setOffice(office);
+
+        us.setFirstName("Евгений");
+        us.setMiddleName("Анатольевич");
+        us.setLastName("Каплан");
+
+        us.setPosition("Агроном");
+
+        String d1 = "21.10.2012";
+        String d2 = "07.10.2009";
+        String d3 = "12.12.2007";
+        Date regDt = null;
+        Date docDt = null;
+        Date docDt2 = null;
+        try {
+            regDt = format.parse(d1);
+            docDt = format.parse(d2);
+            docDt2 = format.parse(d3);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        us.setRegistrationDate(regDt);
+
+        // Загрузка документов
+        // первый документ
+        DocUser docUser = new DocUser();
+        docUser.setDocDate(docDt);
+        docUser.setDocNumber(12123165115165165L);
+
+        Doc doc = docDAO.loadById(21); // справочник документов, берём из базы
+        docUser.setDocs(doc);
+
+        // второй документ
+        DocUser docUser2 = new DocUser();
+        docUser2.setDocDate(docDt2);
+        docUser2.setDocNumber(10000000000065L);
+
+        Doc doc2 = docDAO.loadById(7);
+        docUser2.setDocs(doc2);
+
+        us.addDocUser(docUser);
+        us.addDocUser(docUser2);
+
+        // Загрузка citizenship
+        Citizenship citizen = CitizDAO.loadByCode(398L);
+        us.setCitizenship(citizen);
+
+        userDAO.save(us);
+
+        Assert.assertEquals(INSERT_COUNT_FROM_DB, userDAO.all().size());
+
+        Assert.assertEquals(2, userDAO.loadById(21L).getDocUsers().size());
     }
 
 }
