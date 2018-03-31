16f28b1525b720a034d33d749a900ed7644cc74b
goryacms <gorjachev_maksim@mail.ru>
2018-03-31 15:35:56 +0300

    Убрал лишнее (врапер с list)

--- src/main/java/ru/bellintegrator/practice/util/JSONResponseWrapper.java
+++ src/main/java/ru/bellintegrator/practice/util/JSONResponseWrapper.java
@@ -5,17 +5,14 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
 
-import java.util.List;
 
 @ControllerAdvice
-
 public class JSONResponseWrapper implements ResponseBodyAdvice<Object> {
 
     @Override
@@ -26,21 +23,7 @@ public class JSONResponseWrapper implements ResponseBodyAdvice<Object> {
     @Override
     @SuppressWarnings("unchecked")
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body instanceof List) {
-            return new Wrapper<>((List<Object>) body);
-        }
-
         return new WrapperObj<Object>(body);
-    }
-
-    @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-    @JsonSerialize
-    private class Wrapper<T> {
-        private final List<T> data;
-
-        public Wrapper(List<T> data) {
-            this.data = data;
-        }
     }
 
     @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
